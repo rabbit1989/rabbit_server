409b386d137b5e9131d61f1acc17cef493c553cb
rabbit1989 <galaxysf3@gmail.com>
2015-08-27 19:05:52 +0800

    add rpc channel

--- src/rpc/rpc_channel.hpp
+++ src/rpc/rpc_channel.hpp
@@ -19,76 +19,21 @@ FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TOR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 *******************************************************************************************
-       header of socket wrapper class: this class works on both 
-       windows and unix-like platform
+       						header of rpc channel
 *******************************************************************************************/
 
-#ifndef SOCKET_HPP
-#define SOCKET_HPP
 
-#if defined(WIN32)
-	#include <winsock2.h>
-#else
-	#include <sys/types.h>
-    #include <unistd.h>
-    #include <fcntl.h>
-	#include <sys/socket.h>
-    typedef int SOCKET
-#endif
 
-#include <string>
+#ifndef RPC_CHANNEL_HPP
+#define RPC_CHANNEL_HPP
+
 #include "uncopyable.hpp"
 
-namespace rabbit{
-
-typedef std::pair<std::string, u_int> sock_addr;
-	
-class socket{
+class rpc_channel: uncopyable{
 public:
-	socket(int, int, int);
-	socket(): _socket(SOCKET_ERROR){};
-	socket(SOCKET);
-	socket(const socket&);
-	socket& operator=(const socket&);
-	virtual ~socket();
-	socket accept(struct sockaddr*, int*);
-	int listen(int);
-	int bind(const struct sockaddr*, int);
-	int	connect(const struct sockaddr*, int);
-	ssize_t send(const char*, size_t, int);
-	ssize_t recv(char*, size_t, int);
-	int shutdown(int);
-	int close();
-	bool is_valid();
-
-	//set socket to blocking/nonblocking mode
-	//0 for blocking and 1 for nonblocking
-	int set_nonblocking(u_long);
-	
-	//set err_code
-	void set_errno();
-	
-	//return the latest error code
-	int get_errno();
-	
-	const std::string& get_ip() const;
-	const u_int get_port() const; 
-	
-	//return address pair()
-	const sock_addr& get_addr() const;
 
 private:
-	SOCKET get_rawsocket() const;
 
-	void set_rawsocket(SOCKET);
-	
-private:
-	sock_addr _addr;
-	SOCKET _socket;
-	int _state;
-	int _err_code;
 };
-
-}
 
 #endif
